5736877bc4928c450f4bfadb0b236636c7bd87d1
thuannd97 <nguyenducthuan091997@gmail.com>
2021-09-14 21:52:36 +0700

    guessing game

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,7 @@
 // create project: cargo new project_name --bin
 // run project: cargo run
-// build project: cargo build
-// build release project: cargo build --release
+// build project: cargo build ->> target/debug
+// build release project: cargo build --release ->> target/release
 fn main() {
     println!("Hello, world!");
 }
